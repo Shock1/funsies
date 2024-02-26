e11d2a19d6e274ed20f2eb46510c2d416e5c2b23
Shock1 <58305237+Shock1@users.noreply.github.com>
2024-02-26 16:38:53 -0500

    added a btn in a separate page

---
+++ btn.js
@@ -0,0 +1,10 @@
+console.log("Connected to HTML!");
+
+$(".hero-btn").click(function () {
+  if (!$(".hero-btn").parent().hasClass("active")) {
+    $(this).parent().stop().addClass("active");
+    setTimeout(function () {
+      $(".hero-btn").parent().removeClass("active");
+    }, 2000);
+  }
+});
