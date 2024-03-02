22416411ce803c7733dbc7691ad76e5434b51f7b
Shock1 <58305237+Shock1@users.noreply.github.com>
2024-03-02 14:14:58 -0500

    Fixed up spacing and trying authentication again

---
+++ main/script.js
@@ -0,0 +1,9 @@
+      // Check if the user is authenticated
+      console.log("authenticated");
+      var authenticated = sessionStorage.getItem("authenticated");
+       // Log the value of the authenticated variable
+      if (!authenticated || authenticated !== "true") {
+      // If not authenticated, redirect to the landing page
+        window.location.href = "../index.html";
+        console.log("not authenticated");
+      }
