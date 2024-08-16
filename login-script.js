3a841fc59a2a29741d16652310e633b3ce530486
Shock1 <58305237+Shock1@users.noreply.github.com>
2024-08-15 20:37:34 -0400

    Added constitution logo, button to login page

--- login-script.js
+++ login-script.js
@@ -24,11 +24,23 @@ $(".hero-btn .btn").click(function () {
     }
 });
 
-$('.question-mark, .tracker-mark').click(function () {
-    var bgColor = $(this).hasClass('question-mark') ? '#FFE3EC' : '#57008a';
-    var page = $(this).hasClass('question-mark') ? 'mail/mail.html' : 'tracker/index.html';
+$('.question-mark, .tracker-mark, .constitution-mark').click(function () {
+    var bgColor = $(this).hasClass('question-mark') ? '#FFE3EC' : 
+                  $(this).hasClass('tracker-mark') ? '#57008a' : 
+                  '#272824';
+    var page = $(this).hasClass('question-mark') ? 'mail/mail.html' : 
+               $(this).hasClass('tracker-mark') ? 'tracker/index.html' : 
+               'constitution/index.html';
     fadeOutContentAndRedirect(page, bgColor);
 });
+
+
+
+// $('.question-mark, .tracker-mark').click(function () {
+//     var bgColor = $(this).hasClass('question-mark') ? '#FFE3EC' : '#57008a';
+//     var page = $(this).hasClass('question-mark') ? 'mail/mail.html' : 'tracker/index.html';
+//     fadeOutContentAndRedirect(page, bgColor);
+// });
 
 function fadeOutContentAndRedirect(page, bgColor) {
     $('#first-page-content').fadeOut(1000, function () {
