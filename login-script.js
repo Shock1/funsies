b2b3c10a1980d2e267a4068843ef6e845bedcf67
Shock1 <58305237+Shock1@users.noreply.github.com>
2024-12-18 22:15:54 -0500

    transition color fix

--- login-script.js
+++ login-script.js
@@ -26,7 +26,7 @@ $(".hero-btn .btn").click(function () {
 
 $('.question-mark, .tracker-mark, .constitution-mark').click(function () {
     var bgColor = $(this).hasClass('question-mark') ? '#FFE3EC' : 
-                  $(this).hasClass('tracker-mark') ? '#57008a' : 
+                  $(this).hasClass('tracker-mark') ? '#8a003f4f' : 
                   '#272824';
     var page = $(this).hasClass('question-mark') ? 'mail/mail.html' : 
                $(this).hasClass('tracker-mark') ? 'tracker/index.html' : 
@@ -37,7 +37,7 @@ $('.question-mark, .tracker-mark, .constitution-mark').click(function () {
 
 
 // $('.question-mark, .tracker-mark').click(function () {
-//     var bgColor = $(this).hasClass('question-mark') ? '#FFE3EC' : '#57008a';
+//     var bgColor = $(this).hasClass('question-mark') ? '#FFE3EC' : '#8a003f4f';
 //     var page = $(this).hasClass('question-mark') ? 'mail/mail.html' : 'tracker/index.html';
 //     fadeOutContentAndRedirect(page, bgColor);
 // });
